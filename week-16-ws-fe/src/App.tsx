e0e74a09a0d92cc212eab6a102eb506daa12cc85
avigupta250 <avinashkumarnav52@gmail.com>
2024-12-04 20:15:02 +0530

    created useSocket Hook

--- week-16-ws-fe/src/App.tsx
+++ week-16-ws-fe/src/App.tsx
@@ -1,29 +1,32 @@
 import { useEffect, useRef, useState } from 'react'
 
 import './App.css'
+import { useSocket } from './hooks/useScoket';
 
 function App() {
 
 
 const inputRef=useRef();
 
-const [socket, setSocket] = useState<WebSocket | null>(null);
+// const [socket, setSocket] = useState<WebSocket | null>(null);
+
+const {send}=useSocket({url:'ws://localhost:8080'})
 
   function sendMessage(){
     // @ts-ignore
   const message=inputRef.current.value;
-  socket?.send(message);
+  send(message);
       
   }
 
-  useEffect(()=>{
-    const ws = new WebSocket("ws://localhost:8080");
-    setSocket(ws);
+  // useEffect(()=>{
+  //   const ws = new WebSocket("ws://localhost:8080");
+  //   setSocket(ws);
 
-    ws.onmessage = (ev) => {
-      console.log('Message received:', ev.data);
-    }
-  },[])
+  //   ws.onmessage = (ev) => {
+  //     console.log('Message received:', ev.data);
+  //   }
+  // },[])
 
   return (
     <>
---
+++ week-16-ws-fe/src/hooks/useScoket.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+
+
+
+export function useSocket({url}:any){
+
+    const [socket,setSocket]=useState<WebSocket|null>(null);
+
+console.log(url)
+    useEffect(()=>{
+        const ws=new WebSocket(url);
+        setSocket(ws);
+
+        ws.onmessage = (ev) => {
+                console.log('Message received:', ev.data);
+              }
+
+        return ()=>{
+
+            socket?.close();
+            console.log("Unmmounting ")
+        }
+    },[])
+
+    const send=(data:any)=>{
+        socket?.send(data)
+    }
+
+
+    return {socket,send}
+}
