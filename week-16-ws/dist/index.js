6145ed511a9ef964a0334472f582fd8c22c3c50b
avigupta250 <avinashkumarnav52@gmail.com>
2024-12-05 13:48:32 +0530

    backend chat app

--- week-16-ws/dist/index.js
+++ week-16-ws/dist/index.js
@@ -2,11 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // console.log("hii")
 const ws_1 = require("ws");
+// https://projects.100xdevs.com/tracks/ABEC/ABEC-3 
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-// event Handler
+let allSocket = [];
 wss.on("connection", function (socket) {
-    socket.send("hello ");
-    socket.on("message", (e) => {
-        console.log(e.toString());
+    socket.on('message', (ev) => {
+        var _a;
+        //  @ts-ignore
+        const parseData = JSON.parse(ev);
+        console.log(parseData);
+        if (parseData.type === 'join') {
+            allSocket.push({ socket: socket, roomId: parseData.payload.roomId });
+        }
+        if (parseData.type === 'chat') {
+            const currentUserRoom = (_a = allSocket.find((x) => x.socket == socket)) === null || _a === void 0 ? void 0 : _a.roomId;
+            for (let i = 0; i < allSocket.length; i++) {
+                if (allSocket[i].roomId == currentUserRoom) {
+                    allSocket[i].socket.send(parseData.payload.message);
+                }
+            }
+        }
     });
+    socket.send("hello ");
 });
--- week-16-ws/src/index.ts
+++ week-16-ws/src/index.ts
@@ -1,16 +1,42 @@
 // console.log("hii")
-import { WebSocketServer } from "ws"
+import { WebSocketServer,WebSocket } from "ws"
 
-
+// https://projects.100xdevs.com/tracks/ABEC/ABEC-3 
 const wss=new WebSocketServer({port:8080});
 
 // event Handler
+interface User{
+    socket:WebSocket,
+    roomId:string
+}
+let allSocket:User[]=[];
+
+
 wss.on("connection",function(socket){
+
+    socket.on('message',(ev)=>{
+    //  @ts-ignore
+        const parseData=JSON.parse(ev);
+        console.log(parseData)
+
+        if(parseData.type==='join'){
+            allSocket.push({socket:socket,roomId:parseData.payload.roomId})
+        }
+
+        if(parseData.type==='chat'){
+            const currentUserRoom=allSocket.find((x)=>x.socket==socket)?.roomId
+
+            for(let i=0;i<allSocket.length;i++){
+               
+                if(allSocket[i].roomId==currentUserRoom)
+                {
+                    allSocket[i].socket.send(parseData.payload.message)
+                }
+            }
+        }
+
+    })
 
     socket.send("hello ")
    
-    socket.on("message",(e)=>{
-        console.log(e.toString())
-    })
-
 })
