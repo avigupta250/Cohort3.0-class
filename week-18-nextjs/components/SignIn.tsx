557783bb9ebb87ce4b98a5a3cb20730e265181ae
avigupta250 <avinashkumarnav52@gmail.com>
2024-12-19 21:08:09 +0530

    remove button

--- week-18-nextjs/components/SignIn.tsx
+++ week-18-nextjs/components/SignIn.tsx
@@ -11,15 +11,11 @@ export function Signin() {
                     <div className="pt-2">
                         <LabelledInput label="Username" placeholder="harkirat@gmail.com" />
                         <LabelledInput label="Password" type={"password"} placeholder="123456" />
-                        {/* <button onClick={()=>console.log("hello")} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button> */}
-                    </div>
+                           </div>
                 </div>
             </a>
         </div>
-        {/* <button onClick={() => {
-    console.log("User clicked on signin")
-}} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Hello</button> */}
-
+       
     </div>
 }
 
